84b168cb7e3ddee64c7b0415729447434e51f63d
Svetlana <se55petrova@gmail.com>
2025-04-24 11:27:11 +0500

    Добавила верстку (обычная), вынесла компоненты шапки, сортировки, карточки с пиццей, добавила файлы со стилями scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-function App() {
+import React from 'react';
+import './scss/app.scss';
+import HeaderComponent from './components/HeaderComponent';
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import PizzaComponent from './components/PizzaComponent';
+
+function App() { 
   return (
-    <div className="App">
-      
+    <div class='wrapper'>
+      <HeaderComponent />
+      <div class='content'>
+        <div class='container'>
+          <div class='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class='content__title'>Все пиццы</h2>
+          <div class='content__items'>
+            <PizzaComponent title={'Пицца'} price={500}/>
+            <PizzaComponent title={'Пицца2'} price={5000}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
