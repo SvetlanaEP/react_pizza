47296e7498b9264db63a169824b5dcf27111530a
Svetlana <se55petrova@gmail.com>
2025-05-01 14:32:00 +0500

    Рендер категорий через map, смена класса выбранной категории по клику

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
+
 export default function Categories() {
+  const [activeIndex, setActiveIndex]  = useState(0)
+  const categoriesList = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  }
   return (
     <div className='categories'>
       <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categoriesList.map((item, index) => (
+          <li key={index} className={activeIndex === index ? 'active' : ''} 
+          onClick={() => onClickCategory(index)}>{item}</li>
+        ))}
+
       </ul>
     </div>
   );
