33f8b625b4d4a34f6ee15a288cecc722646da8ed
Svetlana <se55petrova@gmail.com>
2025-05-02 15:43:33 +0500

    Загрузка пицц с mockapi через хук useEffect (вместо моковых даннных)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scss/app.scss';
 import HeaderComponent from './components/HeaderComponent';
 import Sort from './components/Sort';
 import Categories from './components/Categories';
-import PizzaComponent from './components/PizzaComponent';
+import PizzaComponent from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
+//import pizzas from './assets/pizzas.json';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://68149373225ff1af16294cea.mockapi.io/items')
+    .then((res) => {
+      return res.json();
+    })
+    .then((arr) => {
+      setItems(arr);
+    });
+  }, [])
+
   return (
     <div className='wrapper'>
       <HeaderComponent />
@@ -19,10 +31,10 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {pizzas.map((pizza) => (
+            {items.map((pizza) => (
               <PizzaComponent
                 key={pizza.id}
-               {...pizza}
+                {...pizza}
               />
             ))}
           </div>
